dd1254d952554875ee6403a99f1f18cf295d715a
JRBarreraM <35788695+JRBarreraM@users.noreply.github.com>
2018-11-11 21:45:46 -0400

    Implements Movement,InputText and Dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour 
+{
+	public GameObject dBox;
+	public GameObject tBox;
+	public Text dText;
+	public bool dBoxActive;
+	// Use this for initialization
+	void Start () 
+	{
+		dBoxActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (dBoxActive && Input.GetKeyDown (KeyCode.Return))
+		{
+			dBox.SetActive (false);
+			tBox.SetActive (false);
+			dBoxActive = false;
+		}
+	}
+
+	public void showBox(string dialogue)
+	{
+		dBox.SetActive (true);
+		tBox.SetActive (true);
+		dBoxActive = true;
+		dText.text = dialogue;
+	}
+}
---
+++ Assets/Scripts/keyCheckerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class keyCheckerScript : MonoBehaviour 
+{
+	public string[] Respuestas;
+	[SerializeField]
+	private InputField input;
+	public void GetInput(string code)
+	{
+		if (code.Equals ("shame"))
+		{
+			//no estoy claro
+		}
+		for (int i = 0; i < Respuestas.Length; i++)
+		{
+			if (code.Equals(Respuestas[i]))
+			{
+				Respuestas [i] = "";
+				Debug.Log ("Correcto");
+				//Aqui Carlos Aumenta la Barra de hackeo
+				//Aqui se manda el mensaje para cambiar de color la pantalla
+				input.text = "";
+				return;
+			}
+		}
+		Debug.Log ("Incorrecto");
+		return;
+	}
+}

70e4f109eebfb7dc9119c7d4acd7c137fed98c87
JRBarreraM <15-10123@usb.ve>
2018-11-24 12:08:09 -0400

    Desactivar Despues De Hablar

---
+++ Assets/tutorialManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialManager : MonoBehaviour {
+	public GameObject[] popUps;
+	public int popUpIndex;
+	void Update()
+	{
+		for (int i = 0; i < popUps.Length; i++)
+		{
+			if (i == popUpIndex)
+			{
+				popUps [i].SetActive (true);
+			} 
+			else
+			{
+				popUps [i].SetActive (false);
+			}
+		}
+		if (popUpIndex == 0)
+		{
+			if (Input.GetKeyDown (KeyCode.LeftArrow))
+			{
+				
+			}
+		}
+	}
+}

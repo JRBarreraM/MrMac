be9660b73c66cccf717d7b8e6a61fa1aef54cad4
JRBarreraM <15-10123@usb.ve>
2018-11-25 03:58:23 -0400

    ultima version

--- Assets/Scripts/Dialogue/DialogueHolder.cs
+++ Assets/Scripts/Dialogue/DialogueHolder.cs
@@ -6,7 +6,10 @@ public class DialogueHolder : MonoBehaviour
 {
 	private DialogueManager dManager;
 	public string[] dialogueLines;
+	public string[] dialogueLinesAlter;
+	private string[] RespuestasRetos;
 	public string caller;
+	private bool alter=false;
 	// Use this for initialization
 	void Start () 
 	{
@@ -21,11 +24,23 @@ public class DialogueHolder : MonoBehaviour
 			{
 				if (caller.Equals ("FinalPC"))
 				{
+					RespuestasRetos = GameObject.Find ("TerminalController").GetComponent<keyCheckerScript>().RespuestasRetos;
 					GameObject.Find("TerminalController").GetComponent<keyCheckerScript>().aM.Stop("NormalCorrect");
+					for (int i = 0; i < RespuestasRetos.Length; i++)
+					{
+						if (!(RespuestasRetos [i].Equals ("1er1g16g156eg16vdv1")))
+						{
+							alter = true;
+						}
+					}
 				}
 				if (!(dManager.dBoxActive))
 				{
-					dManager.dialogLines = dialogueLines;
+					if (alter) {
+						dManager.dialogLines = dialogueLinesAlter;
+					} else {
+						dManager.dialogLines = dialogueLines;
+					}
 					dManager.showDialogue ();
 					GameObject.Find ("Player").GetComponent<Movement> ().enabled = false;
 					GameObject.Find ("Player").GetComponent<Animator> ().enabled = false;
